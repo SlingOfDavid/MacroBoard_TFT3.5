c002f18951e1bfb31bf8d21391789b8a4e9f744a
Vinicius Correia <vc.extramail@gmail.com>
2026-07-20 16:09:26 -0300

    old project cleanup

--- MacroBoard/src_macro/macro_logic.h
+++
@@ -1,7 +0,0 @@
-#ifndef MACRO_LOGIC_H
-#define MACRO_LOGIC_H
-
-void init_macro_logic();
-void execute_macro(int index);
-
-#endif
--- MacroBoard/src_macro/web_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef WEB_SERVER_H
-#define WEB_SERVER_H
-
-void start_web_server();
-
-#endif
