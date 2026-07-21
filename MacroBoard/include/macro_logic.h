8d04ff9e60922d89387d343ba1c7177457d760e5
Vinicius Correia <vc.extramail@gmail.com>
2026-07-21 15:30:32 -0300

    expanded "if (debug) to all serial prints

--- MacroBoard/include/macro_logic.h
+++ MacroBoard/include/macro_logic.h
@@ -6,10 +6,12 @@ void execute_macro(int index);
 bool is_ble_connected();
 void apply_settings();
 
-// AFK and Theme logic
+// AFK, Theme and Debug logic
 bool is_afk_enabled();
 void set_afk_enabled(bool enabled);
 void process_afk_logic();
 int get_tui_color_index();
+bool is_debug_enabled();
+void set_debug_enabled(bool enabled);
 
 #endif
